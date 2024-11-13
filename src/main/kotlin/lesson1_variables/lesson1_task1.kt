3115120bba73b549667b01943592e9b0a3b51581
vlads <quietDoppio@yandex.ru>
2024-11-13 12:41:30 +0300

    KSL-1-1 variables were created and assigned a value

---
+++ src/main/kotlin/lesson1_variables/lesson1_task1.kt
@@ -0,0 +1,7 @@
+package lesson1_variables
+
+fun main(){
+    val countOfCurrentOrders: Byte = 75
+    val thanksMessageToClient = "Благодарим за покупку"
+    println("Кол-во рабочих заказов: $countOfCurrentOrders, сообщение клиенту: $thanksMessageToClient")
+}
