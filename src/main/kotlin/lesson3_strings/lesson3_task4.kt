0fc7a3f310eef7f284181b5919a91ce077eaec92
vlads <quietDoppio@yandex.ru>
2024-11-13 20:43:09 +0300

    KSL-3-3 multiplication table has been created

---
+++ src/main/kotlin/lesson3_strings/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package lesson3_strings
+
+fun main(){
+    var moveCount = 1
+    var fromPosition = "E2"
+    var toPosition = "E4"
+
+        println("ход: $fromPosition - $toPosition, номер хода: $moveCount")
+
+    fromPosition = "D2"
+    toPosition = "D3"
+
+        println("ход: $fromPosition - $toPosition, номер хода: ${++moveCount}")
+
+}
