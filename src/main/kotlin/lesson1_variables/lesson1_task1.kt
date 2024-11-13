75053906e79ba5631c111638399aed81be3aac53
vlads <quietDoppio@yandex.ru>
2024-11-13 15:53:31 +0300

    KSL-1-1 variables have been refactored

--- src/main/kotlin/lesson1_variables/lesson1_task1.kt
+++ src/main/kotlin/lesson1_variables/lesson1_task1.kt
@@ -1,7 +1,7 @@
 package lesson1_variables
 
 fun main(){
-    val countOfCurrentOrders: Byte = 75
-    val thanksMessageToClient = "Благодарим за покупку"
+    val countOfCurrentOrders: Short = 75
+    val thanksMessageToClient: String = "Благодарим за покупку"
     println("Кол-во рабочих заказов: $countOfCurrentOrders, сообщение клиенту: $thanksMessageToClient")
 }
