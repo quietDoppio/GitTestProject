edcb65274367101573bd5fc92780a58101eda688
vlads <quietDoppio@yandex.ru>
2024-11-13 13:22:09 +0300

    KSL-1-3 variables have been created, initialized, changed and printed

---
+++ src/main/kotlin/lesson1_variables/lesson1_task3.kt
@@ -0,0 +1,17 @@
+package lesson1_variables
+
+fun main(){
+    val year: Short = 1961
+    var hour: Byte = 9
+    var minute: Byte = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print("$hour:")
+    print(minute)
+}
