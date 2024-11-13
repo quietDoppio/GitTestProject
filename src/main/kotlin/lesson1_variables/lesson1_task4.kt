f6133e7f85e4a64b758855cd83aceb515aa2b237
vlads <quietDoppio@yandex.ru>
2024-11-14 00:07:00 +0300

    KS-1-4 apogee refactored

--- src/main/kotlin/lesson1_variables/lesson1_task4.kt
+++ src/main/kotlin/lesson1_variables/lesson1_task4.kt
@@ -6,7 +6,7 @@ fun main(){
     val partOfDay: Float = 0.075F
     val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
-    val apogee: Short = 32700
+    val apogee: Int = 327000
 
     println("""
         $length
