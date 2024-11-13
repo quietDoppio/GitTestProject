31b8c8857d61880d8e6819a59ee4cf2f5fff14f7
vlads <quietDoppio@yandex.ru>
2024-11-13 20:16:24 +0300

    KSL-3-2 surname has been changed

---
+++ src/main/kotlin/lesson3_strings/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package lesson3_strings
+
+fun  main(){
+
+        val surname = "Андреева"
+        val name = "Татьяна"
+        val patronymic = "Сергеевна"
+        val age = 20
+        println("$surname $name $patronymic, $age")
+
+        val newSurname = "Сидорова"
+        val newAge = 22
+        println("$newSurname $name $patronymic, $newAge")
+
+
+}
