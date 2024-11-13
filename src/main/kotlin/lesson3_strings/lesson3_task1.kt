de7332e8bfd01aa0b8fd176bba08818c695067e8
vlads <quietDoppio@yandex.ru>
2024-11-13 20:06:50 +0300

    KSL-3-1 greetings created

---
+++ src/main/kotlin/lesson3_strings/lesson3_task1.kt
@@ -0,0 +1,11 @@
+package lesson3_strings
+
+fun main(){
+    val userName = "Иван Иванович"
+    var greeting = "Добрый день $userName"
+        println(greeting)
+
+        greeting = "Добрый вечер $userName"
+        println(greeting)
+
+}
