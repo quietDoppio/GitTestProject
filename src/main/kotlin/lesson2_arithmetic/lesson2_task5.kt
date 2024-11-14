47ca57e934b806bc7c42fc43480207f229e16b01
vlads <quietDoppio@yandex.ru>
2024-11-14 15:06:32 +0300

    KS-2-5 compound interest has been calculated

--- src/main/kotlin/lesson2_arithmetic/lesson2_task5.kt
+++ src/main/kotlin/lesson2_arithmetic/lesson2_task5.kt
@@ -3,11 +3,18 @@ package lesson2_arithmetic
 import kotlin.math.pow
 import kotlin.math.round
 
-fun main(){
-    val clientEnteredSum = 70_000
-    val interestRate = 16.7 / 100
-    val countOfYears= 20
+const val ROUNDING_SCALE = 1000
+const val BASE_AMOUNT = 1
+const val PERCENT_SCALE = 100
 
-    val finalSum = round((clientEnteredSum * (1+interestRate).pow(countOfYears)) * 1000) / 1000
-    println(finalSum)
+fun main() {
+    val clientEnteredSum = 70_000
+    val interestRate = 16.7 / PERCENT_SCALE
+    val countOfYears = 20
+
+    val finalSum = round(
+        (clientEnteredSum * (BASE_AMOUNT + interestRate).pow(countOfYears)) * ROUNDING_SCALE
+    ) / ROUNDING_SCALE
+
+        println(finalSum)
 }
