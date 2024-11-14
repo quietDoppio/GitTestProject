b7f3523ee124ff640754c614ef4d3e732c0f9481
vlads <quietDoppio@yandex.ru>
2024-11-14 14:48:40 +0300

    KS-2-4 buff calculated

--- src/main/kotlin/lesson2_arithmetic/lesson2_task4.kt
+++ src/main/kotlin/lesson2_arithmetic/lesson2_task4.kt
@@ -1,12 +1,15 @@
 package lesson2_arithmetic
 
+const val PERCENT_SCALE = 100.0
+
 fun main(){
     val crystalOreCount = 7
     val metalOreCount = 11
-    val buffPercent = 0.20
+    val buffPercent = 20 / PERCENT_SCALE
 
     val crystalOreBuffedResources = (buffPercent * crystalOreCount).toInt()
     val metalOreBuffedResources = (buffPercent * metalOreCount).toInt()
+
     println("""
         Бонусная кристаллическая руда - $crystalOreBuffedResources
         Бонусная железная руда - $metalOreBuffedResources
