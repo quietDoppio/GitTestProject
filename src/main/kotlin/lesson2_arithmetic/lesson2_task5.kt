de3f9241aa2dd13e8bcabe22d6a9388be66a5622
vlads2 <quietDoppio@yandex.ru>
2024-11-15 15:54:14 +0300

    KS-2-5 Переделанный калькулятор сложного процента

--- src/main/kotlin/lesson2_arithmetic/lesson2_task5.kt
+++ src/main/kotlin/lesson2_arithmetic/lesson2_task5.kt
@@ -1,20 +1,17 @@
 package lesson2_arithmetic
 
 import kotlin.math.pow
-import kotlin.math.round
 
-const val ROUNDING_SCALE = 1000
 const val BASE_AMOUNT = 1
 const val PERCENT_SCALE = 100
 
 fun main() {
-    val clientEnteredSum = 70_000
-    val interestRate = 16.7 / PERCENT_SCALE
-    val countOfYears = 20
+    val clientEnteredAmount = 70_000
+    val interestRate = 16.7
+    val years = 20
 
-    val finalSum = round(
-        (clientEnteredSum * (BASE_AMOUNT + interestRate).pow(countOfYears)) * ROUNDING_SCALE
-    ) / ROUNDING_SCALE
+    val finalSum =
+        (clientEnteredAmount * (BASE_AMOUNT + (interestRate / PERCENT_SCALE)).pow(years))
 
-        println(finalSum)
+        println("%.3f".format(finalSum))
 }
