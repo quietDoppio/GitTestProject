a2ceaea461f8512e876bec82f30ec547430f47f8
vlads2 <quietDoppio@yandex.ru>
2024-11-16 19:49:35 +0300

    KS-5-4 Логин аккаунта

---
+++ src/main/kotlin/lesson5_if_Else/lesson5_task4.kt
@@ -0,0 +1,22 @@
+package lesson5_if_Else
+
+
+fun main() {
+    val user9221Name = "Zaphod"
+    val user9221Password = "PanGalactic"
+
+    print("Введите логин: ")
+    val userName = readln()
+
+    if (userName == user9221Name) {
+        print("Введите пароль: ")
+        val userPassword = readln()
+        if (userPassword == user9221Password) {
+            println("Добро пожаловать на борт")
+        } else {
+            println("Неверный пароль")
+        }
+    } else {
+        println("Такой учётной записи не существует. Пожалуйста зарегистрируйтесь")
+    }
+}
