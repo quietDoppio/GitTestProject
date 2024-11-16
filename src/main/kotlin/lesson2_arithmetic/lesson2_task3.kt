776a0d10d52204520d87cacc590342bca105bcb8
vlads2 <quietDoppio@yandex.ru>
2024-11-16 18:48:38 +0300

    KS-2-3 Переделал функцию для расчёта

--- src/main/kotlin/lesson2_arithmetic/lesson2_task3.kt
+++ src/main/kotlin/lesson2_arithmetic/lesson2_task3.kt
@@ -16,5 +16,5 @@ fun main() {
     val arrivalTimeHour = (totalTimeSeconds / SECONDS_IN_HOUR) % HOURS_IN_DAY
     val arrivalTimeMinutes = (totalTimeSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
 
-    println("$arrivalTimeHour:$arrivalTimeMinutes")
+    println(String.format("%02d:%02d", arrivalTimeHour, arrivalTimeMinutes))
 }
