ab70a4fcaa749e6e2abff1e6649d2b94fa70eb5a
vlads <quietDoppio@yandex.ru>
2024-11-17 22:36:20 +0300

    KS-2-4 branch changed const

--- src/main/kotlin/lesson2_arithmetic/lesson2_task4.kt
+++ src/main/kotlin/lesson2_arithmetic/lesson2_task4.kt
@@ -1,14 +1,14 @@
 package lesson2_arithmetic
 
-const val PERCENT_SCALE = 100.0
+const val PERCENT_SCALE_DOUBLE = 100.0
 
 fun main(){
     val crystalOreCount = 7
     val metalOreCount = 11
     val buffPercent = 20
 
-    val crystalOreBuffedResources = ((buffPercent / PERCENT_SCALE) * crystalOreCount).toInt()
-    val metalOreBuffedResources = ((buffPercent / PERCENT_SCALE)  * metalOreCount).toInt()
+    val crystalOreBuffedResources = ((buffPercent / PERCENT_SCALE_DOUBLE) * crystalOreCount).toInt()
+    val metalOreBuffedResources = ((buffPercent / PERCENT_SCALE_DOUBLE)  * metalOreCount).toInt()
 
     println("Бонусная кристаллическая руда - $crystalOreBuffedResources")
     println("Бонусная железная руда - $metalOreBuffedResources")
