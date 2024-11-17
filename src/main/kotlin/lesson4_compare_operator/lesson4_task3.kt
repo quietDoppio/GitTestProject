af20adc56d50ebfe6549846084fe8a5284e52159
vlads <quietDoppio@yandex.ru>
2024-11-17 18:39:49 +0300

    KS-4-3 Переставил операторы &&

--- src/main/kotlin/lesson4_compare_operator/lesson4_task3.kt
+++ src/main/kotlin/lesson4_compare_operator/lesson4_task3.kt
@@ -4,18 +4,16 @@ const val REQUIRED_AIR_HUMIDITY = 20
 const val WRONG_SEASON = "зима"
 
 fun main() {
-    val isWeatherSunny: Boolean = true
-    val isTentOpen: Boolean = true
-    val airHumidity: Int = 20
-    val currentSeason: String = "зима"
+    val isWeatherSunny = true
+    val isTentOpen = true
+    val airHumidity = 20
+    val currentSeason = "зима"
 
     val isConditionsGood =
-        isWeatherSunny
-                && isTentOpen
-                && airHumidity == REQUIRED_AIR_HUMIDITY
-                && currentSeason != WRONG_SEASON
+        isWeatherSunny &&
+                isTentOpen &&
+                airHumidity == REQUIRED_AIR_HUMIDITY &&
+                currentSeason != WRONG_SEASON
 
-    println(
-        "Благоприятные ли условия сейчас для роста бобовых? $isConditionsGood"
-    )
+    println("Благоприятные ли условия сейчас для роста бобовых? $isConditionsGood")
 }
