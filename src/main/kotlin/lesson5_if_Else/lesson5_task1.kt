15deab5a85fd3a15c778e8d37586fc9db13691e2
vlads <quietDoppio@yandex.ru>
2024-11-18 12:12:45 +0300

    KS-5-1 вынес переменные и высчитанный результат в отдельные переменные

--- src/main/kotlin/lesson5_if_Else/lesson5_task1.kt
+++ src/main/kotlin/lesson5_if_Else/lesson5_task1.kt
@@ -1,9 +1,13 @@
 package lesson5_if_Else
 
 fun main() {
+    val firstSummand = 100
+    val secondSummand = 80
+    val multiplayer = 2
 
-    print("Решите задачу что бы мы знали что вы не робот: 100 + 40 * 2 = ")
-    val correctAnswer = 180
+    val correctAnswer = 100 + 80 * 2
+
+    print("Решите задачу что бы мы знали что вы не робот: $firstSummand + $secondSummand * $multiplayer = ")
 
     val userDigit = readln().toInt()
 
