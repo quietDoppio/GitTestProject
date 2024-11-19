15e81e28061133f7ccf6c252cb3460c45621ba98
vlads2 <quietDoppio@yandex.ru>
2024-11-19 12:50:00 +0300

    KS-5-5 Поменял if else на when

--- src/main/kotlin/lesson5_if_Else/lesson5_task5.kt
+++ src/main/kotlin/lesson5_if_Else/lesson5_task5.kt
@@ -14,18 +14,18 @@ fun main() {
     print("Введите третье число от 0 до 42: ")
     userGuessList.add(readln().toInt())
 
-    //intersect удаляет одинаковые элементы из за того что возвращает Set.
-    //шанс поймать одинаковые числа мал но он есть. Я что то упустил?
     val matchesSize = digitsToWin.intersect(userGuessList).size
 
     val extraWin = matchesSize == digitsToWin.size
     val bigWin = matchesSize == 2
     val smallWin = matchesSize == 1
 
-    if (extraWin) println("Поздравляю! Вы угадали все числа и выиграли джекпот.")
-    else if (bigWin) println("Поздравляю! Вы угадали два числа и получаете крупный приз.")
-    else if (smallWin) println("Вам выплачивается утешительный приз.")
-    else println("Неудача. Вы не угадали ни одного числа.")
+    when {
+        extraWin -> println("Поздравляю! Вы угадали все числа и выиграли джекпот.")
+        bigWin -> println("Поздравляю! Вы угадали два числа и получаете крупный приз.")
+        smallWin -> println("Вам выплачивается утешительный приз.")
+        else -> println("Неудача. Вы не угадали ни одного числа.")
+    }
 
     println("Выиграшные числа - $digitsToWin")
 }
