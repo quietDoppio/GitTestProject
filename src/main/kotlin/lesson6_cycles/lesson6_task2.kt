6a1af1a7cf3d9142eff45100261fbbc3a4ecdef7
vlads2 <quietDoppio@yandex.ru>
2024-12-29 14:01:22 +0300

    KS-6-2 Таймер

---
+++ src/main/kotlin/lesson6_cycles/lesson6_task2.kt
@@ -0,0 +1,16 @@
+package lesson6_cycles
+
+const val MILLS_IN_SECONDS: Long = 1000
+fun main() {
+
+    print("Введите кол-во секунд для отсчёта: ")
+    val seconds = readln().toLong()
+    var counter = 0
+
+    while (counter <= seconds) {
+        println(counter++)
+        Thread.sleep(MILLS_IN_SECONDS)
+    }
+    println("Прошло $seconds секунд")
+
+}
