3bf7b051c868fa0cbeb2d7518470191692fbdd3e
vlads2 <quietDoppio@yandex.ru>
2024-12-30 16:32:05 +0300

    KS-7-1 Генерация пароля. Вынес диапазоны за цикл

--- src/main/kotlin/lesson7_for_range/lesson7_task1.kt
+++ src/main/kotlin/lesson7_for_range/lesson7_task1.kt
@@ -2,11 +2,13 @@ package lesson7_for_range
 
 fun main() {
     val passwordRange = 1..6
+    val charRange = 'a'..'z'
+    val numRange = 0..9
     var generatedPassword = ""
 
     for (i in passwordRange) {
-        if (i % 2 == 0) generatedPassword += ('a'..'z').random()
-        else generatedPassword += (0..9).random()
+        if (i % 2 == 0) generatedPassword += charRange.random()
+        else generatedPassword += numRange.random()
     }
     println(generatedPassword)
 }
