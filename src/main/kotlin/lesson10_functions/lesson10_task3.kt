95609fb6d4eac5ddfe470691b00aa3e767841123
vlads2 <quietDoppio@yandex.ru>
2025-01-04 16:33:09 +0300

    KS-10-3 Функции. Генерация пароля/ repeat(), random()

---
+++ src/main/kotlin/lesson10_functions/lesson10_task3.kt
@@ -0,0 +1,19 @@
+package lesson10_functions
+
+fun main() {
+    print("Введите максимальное кол-во символов для пароля: ")
+    val passwordRange = readln().toInt()
+    println(generatePassword(passwordRange))
+}
+
+fun generatePassword(passwordRange: Int): String {
+    val newPassword = StringBuilder()
+    val charsRange = listOf('!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ' ')
+    val digitsRange = '0'..'9'
+
+    repeat(passwordRange) {
+        newPassword.append(if (it % 2 == 0) digitsRange.random() else charsRange.random())
+    }
+
+    return newPassword.toString()
+}
