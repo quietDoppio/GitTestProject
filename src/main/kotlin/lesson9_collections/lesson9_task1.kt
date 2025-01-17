d97d4634fffa1aea983dfaae80061ac28535d806
vlads <quietDoppio@yandex.ru>
2025-01-17 15:45:44 +0300

    KS-9-1 Списки. Убрал лишнии отступы

--- src/main/kotlin/lesson9_collections/lesson9_task1.kt
+++ src/main/kotlin/lesson9_collections/lesson9_task1.kt
@@ -1,13 +1,10 @@
 package lesson9_collections
 
 fun main() {
-
     val pancakesIngredients = listOf("Молоко", "Масло", "Вода", "Соль", "Яйцо", "Сахар", "Мука")
-
     println("В рецепте есть следующие ингредиенты: $pancakesIngredients")
+
     pancakesIngredients.forEach {
         println(it)
     }
-
-
 }
