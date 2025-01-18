e9f80f3119ba870bb09188aef350bc6819bc1d18
vlads2 <quietDoppio@yandex.ru>
2025-01-18 18:55:40 +0300

    KS-9-5 Списки. Переписал с использование sortedSet и другими прикольными штуками

--- src/main/kotlin/lesson9_collections/lesson9_task5.kt
+++ src/main/kotlin/lesson9_collections/lesson9_task5.kt
@@ -1,20 +1,17 @@
 package lesson9_collections
 
 fun main() {
-    val listOfEnteredIngredients = MutableList(5) { "" }
+    val ingredients = sortedSetOf<String>()
 
     println("Введите пять названий ингредиентов: ")
-    var iterator = 0
-    while (iterator in listOfEnteredIngredients.indices) {
-        val userIngredient = readln()
-        if (userIngredient in listOfEnteredIngredients) {
+    while (ingredients.size < 5) {
+        val userIngredient = readln().trim()
+        if (!ingredients.add(userIngredient)) {
             println("Такой ингредиент уже присутствует в списке. Введите другой: ")
-            continue
         }
-        listOfEnteredIngredients[iterator] = userIngredient
-        iterator++
     }
-    listOfEnteredIngredients.sort()
-    listOfEnteredIngredients[0] = listOfEnteredIngredients[0].replaceFirstChar { it.uppercase() }
-    println(listOfEnteredIngredients.joinToString(", "))
+    val ingredientsResult = ingredients.toMutableList().apply {
+        this[0] = this[0].replaceFirstChar { it.uppercase() }
+    }
+    println(ingredientsResult.joinToString(", "))
 }
