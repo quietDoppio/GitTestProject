2170666a5338fc05117a7aa1b10af4e141d924f6
vlads <quietDoppio@yandex.ru>
2025-01-21 15:36:41 +0300

    KS-11-1 Классы. Создал и инициализировал классы, вывел поля на экран

---
+++ src/main/kotlin/lesson_11_classes/lesson11_task1.kt
@@ -0,0 +1,26 @@
+package lesson_11_classes
+
+fun main() {
+    val user1 = UserData(
+        userId = 0,
+        userLogin = "Viktor",
+        userPassword = "1@23i4",
+        userMail = "AlexDarkStalker2008@true.gangst"
+    )
+    val user2 = UserData(
+        userId = 1,
+        userLogin =  "Sasha",
+        userPassword = "0o2p2rff%%@!",
+        userMail = "lala@mail.com"
+    )
+
+    println("${user1.userId}, ${user1.userLogin}, ${user1.userPassword}, ${user1.userMail}")
+    println("${user2.userId}, ${user2.userLogin}, ${user2.userPassword}, ${user2.userMail}")
+}
+
+class UserData(
+    val userId: Int,
+    val userLogin: String,
+    val userPassword: String,
+    val userMail: String,
+)
