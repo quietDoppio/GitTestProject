f2070494d781ffe6ae4436bb144dd6ad31fb3f8c
vlads <quietDoppio@yandex.ru>
2025-01-24 20:33:40 +0300

    KS-13-5 Null Safety. NPE. Функция с отловом ошибки

---
+++ src/main/kotlin/lesson13_null_safety/lesson13_task5.kt
@@ -0,0 +1,39 @@
+package lesson13_null_safety
+
+fun main() {
+    val contact = getContact()
+    contact?.printInfo()
+}
+
+fun getContact(): ContactPhoneDirectory? {
+    try {
+        print("Имя: ")
+        val name = readln()
+
+        print("Номер: ")
+        val number = readln().toLong()
+
+        print("Компания: ")
+        val company = readln()
+
+        val filledContact = ContactPhoneDirectory(name, number, company)
+        return filledContact
+
+    } catch (exc: NumberFormatException) {
+        println(
+            "Что-то пошло не так...\n$exc"
+        )
+    }
+
+    return null
+}
+
+class ContactPhoneDirectory(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String,
+) {
+    fun printInfo() {
+        println("Имя: $name, Номер: $phoneNumber, Компания: ${company ?: "Не указано"}")
+    }
+}
