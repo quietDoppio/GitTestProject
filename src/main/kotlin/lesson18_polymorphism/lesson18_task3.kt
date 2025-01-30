6072d1105297ac7b62485b7d3960503a0f6fdfe6
vlads <quietDoppio@yandex.ru>
2025-01-30 22:32:53 +0300

    KS-18-3 Полиморфизм. добавил недостающую функцию по заданию

--- src/main/kotlin/lesson18_polymorphism/lesson18_task3.kt
+++ src/main/kotlin/lesson18_polymorphism/lesson18_task3.kt
@@ -6,27 +6,25 @@ fun main() {
     val cat = Cat("Кот")
 
     val animalsList: List<Animal> = listOf(fox, cat, dog)
-    animalsList.forEach { it.eatFood() }
+    animalsList.forEach {
+        it.eatFood()
+        it.getSleep()
+    }
 }
 
-open class Animal(val name: String) {
-    open fun eatFood() {}
+abstract class Animal(val name: String) {
+    abstract fun eatFood()
+    open fun getSleep() = println("$name лёг спать\n")
 }
 
 class Fox(name: String) : Animal(name) {
-    override fun eatFood() {
-        println("$name ест ягоды")
-    }
+    override fun eatFood() = println("$name ест ягоды")
 }
 
 class Dog(name: String) : Animal(name) {
-    override fun eatFood() {
-        println("$name ест кость")
-    }
+    override fun eatFood() = println("$name ест кость")
 }
 
 class Cat(name: String) : Animal(name) {
-    override fun eatFood() {
-        println("$name ест рыбу")
-    }
+    override fun eatFood() = println("$name ест рыбу")
 }
