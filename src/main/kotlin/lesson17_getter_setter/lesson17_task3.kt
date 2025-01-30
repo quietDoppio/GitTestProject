d6208c0037d9523851507fe55eb307fcf93b35fa
vlads2 <quietDoppio@yandex.ru>
2025-01-30 11:47:02 +0300

    KS-17-3 Геттер, Сеттер. С помощью геттеров создал условия получения переменных

--- src/main/kotlin/lesson17_getter_setter/lesson17_task3.kt
+++ src/main/kotlin/lesson17_getter_setter/lesson17_task3.kt
@@ -4,21 +4,19 @@ fun main() {
     val newFolder0 = Folder("Папка0", 20, true)
     val newFolder1 = Folder("Папка1", 10, false)
 
-    println(newFolder0.name)
-    println(newFolder1.name)
+    println("${newFolder0.name}, ${newFolder0.filesQuantity}")
+    println("${newFolder1.name}, ${newFolder1.filesQuantity}")
 }
 
 class Folder(
-    _name: String,
-    private val filesQuantity: Int,
+    name: String,
+    filesQuantity: Int,
     private val isSecret: Boolean,
 ) {
-    val name: String = _name
-        get() {
-            return when (isSecret) {
-                true -> "Скрытая папка\nКол-во файлов - 0\n"
-                else -> "Имя папки - $field\nКол-во файлов - $filesQuantity\n"
-            }
-        }
+    val name: String = name
+        get() = if (isSecret) "Скрытая папка" else field
+    val filesQuantity: Int = filesQuantity
+        get() = if (isSecret) 0 else field
+
 }
 
