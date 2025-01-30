a93eb5e357cc8bf6d7b0d161a75aa2b091bce376
vlads2 <quietDoppio@yandex.ru>
2025-01-30 10:39:35 +0300

    KS-16-4 Инкапсуляция. Создал приватные переменные и приватный метод для их редактирования, который вызывается из публичной функции.

--- src/main/kotlin/lesson16_encapsulation/lesson16_task4.kt
+++ src/main/kotlin/lesson16_encapsulation/lesson16_task4.kt
@@ -2,17 +2,28 @@ package lesson16_encapsulation
 
 fun main() {
     val order = InternetOrder(842135512)
-    order.changeOrderStatus(!order.getOrderStatus())
-    println(order.getOrderStatus())
+    order.sendNewStatus(true)
+    order.sendNewStatus(false)
 }
 
 class InternetOrder(
     private val numberOfOrder: Int,
     private var statusOfOrder: Boolean = false,
 ) {
-    fun changeOrderStatus(statusOfOrder: Boolean) {
-        this.statusOfOrder = statusOfOrder
+    fun sendNewStatus(newStatus: Boolean) {
+        println("Заявка на смену статуса заказа: $numberOfOrder отправлена менеджеру")
+        changeOrderStatus(newStatus)
+
     }
 
-    fun getOrderStatus(): Boolean = statusOfOrder
+    private fun changeOrderStatus(newStatus: Boolean) {
+        if (statusOfOrder != newStatus) {
+            statusOfOrder = newStatus
+            println("Заявка принята. Новый статус заказа - $statusOfOrder")
+        } else {
+            println("Заявка отклонена.")
+        }
+    }
+
 }
+
