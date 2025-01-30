8ca9d5d199e71504f3917a4470be2106da5a53e5
vlads2 <quietDoppio@yandex.ru>
2025-01-30 10:51:04 +0300

    KS-16-5 Инкапсуляция. Скрыл логику завершения программы в приватной функции которая определенно вызывается из публичной

--- src/main/kotlin/lesson16_encapsulation/lesson16_task5.kt
+++ src/main/kotlin/lesson16_encapsulation/lesson16_task5.kt
@@ -3,41 +3,44 @@ package lesson16_encapsulation
 const val MAX_HP = 100
 fun main() {
     val ashenOne = Player("Негорящий")
-    while (ashenOne.getHealthLvl() > 0) {
+    while (ashenOne.health > 0) {
         ashenOne.hitPlayer()
+        ashenOne.healPlayer()
     }
 }
 
 class Player(
     val name: String,
-    private var health: Int = 100,
+    private var _health: Int = 100,
     private var impactForce: Int = 25,
 ) {
-    fun getHealthLvl(): Int = health
+    val health get() = _health
 
     fun hitPlayer() {
         val damageLvl = (5..50).random()
-        health -= damageLvl
+        if ((health - damageLvl) < 0) _health = 0
+        else _health -= damageLvl
         println(
             "$name получил урон в размере - $damageLvl\n" +
                     "Текущее здоровье - $health\n"
         )
-        if (health <= 0) killPlayer()
-        else healPlayer()
+        if (health == 0) killPlayer()
     }
 
     fun healPlayer() {
-        val healLvl = (1..25).random()
-        if ((health + healLvl) > MAX_HP) health = MAX_HP
-        else health += healLvl
-        println(
-            "$name изличился на - $healLvl\n" +
-                    "Текущее здоровье - $health\n"
-        )
+        if (health != 0) {
+            val healLvl = (1..25).random()
+            if ((health + healLvl) > MAX_HP) _health = MAX_HP
+            else _health += healLvl
+            println(
+                "$name изличился на - $healLvl\n" +
+                        "Текущее здоровье - $health\n"
+            )
+        }
     }
 
     private fun killPlayer() {
-        health = 0
+        _health = 0
         impactForce = 0
         println("$name погиб.")
     }
