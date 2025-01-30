9091e684d9c28180d4ffcd9a8c2251ca543c390e
vlads <quietDoppio@yandex.ru>
2025-01-30 22:35:41 +0300

    KS-18-5 Полиморфизм. Изменил типы переменных функций

--- src/main/kotlin/lesson18_polymorphism/lesson18_task5.kt
+++ src/main/kotlin/lesson18_polymorphism/lesson18_task5.kt
@@ -2,18 +2,18 @@ package lesson18_polymorphism
 
 fun main() {
     val screen = Screen()
-    screen.draw("Круг", 10, 20)
-    screen.draw("Квадрат", 45.0f, 55.2f, 30, 40)
-    screen.draw(33, 43)
+    screen.draw("Круг", 10.0f, 20)
+    screen.draw("Квадрат", 45.0f, 55, 30, 40)
+    screen.draw(33f, 43)
 }
 
 class Screen() {
-    fun draw(shape: String, x: Int, y: Int) =
+    fun draw(shape: String, x: Float, y: Int) =
         println("Нарисован $shape с координатоми (x: $x, y: $y)")
 
-    fun draw(shape: String, x: Float, y: Float, width: Int, height: Int) =
+    fun draw(shape: String, x: Float, y: Int, width: Int, height: Int) =
         println("Нарисован $shape с координатоми (x: $x, y: $y), ширина: $width, высота: $height")
 
-    fun draw(x: Int, y: Int) =
+    fun draw(x: Float, y: Int) =
         println("Нарисована точка с координатоми (x: $x, y: $y)")
 }
